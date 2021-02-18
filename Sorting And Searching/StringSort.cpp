4b408874921d92820127b3daa3652d242091b615
Ritesh Arora <67755113+sa-ritesh@users.noreply.github.com>
2021-02-18 11:32:09 +0530

    Add files via upload

--- Sorting And Searching/StringSort.cpp
+++ Sorting And Searching/StringSort.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
+bool compare(string a, string b)
+{
+    if(b==a.substr(0,b.length()))
+    {
+
+        return a>b;
+    }
+    else
+    {
+
+        return a<b;
+    }
+}
 int main()
 {
-	int n;
-	cin>>n;
-	string arr[n];
-	for(int i=0;i<n;i++){
+    int n;
+    cin>>n;
+    string arr[n];
+    for(int i=0; i<n; i++)
+    {
         cin>>arr[i];
-	}
-	sort(arr,arr+n);
-    for(int i=0;i<n;i++){
-        if(arr[i+1].substr(0,arr[i].length())==arr[i]){
-          swap(arr[i+1],arr[i]);
-        }
-	}
-	for(int i=0;i<n;i++){
+    }
+    sort(arr,arr+n);
+    sort(arr,arr+n,compare);
+    for(int i=0; i<n; i++)
+    {
         cout<<arr[i]<<endl;
-	}
+    }
+
 }
